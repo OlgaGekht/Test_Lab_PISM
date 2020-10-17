370116ad1096f756e7137b04728c6230a2cfd342
OlgaGekht <44096931+OlgaGekht@users.noreply.github.com>
2020-10-17 13:55:48 +0300

    revert files

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+class User {
+
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  say() {
+    alert(`Пользователь: ${this.name}, возраст: ${this.age} лет`);
+  }
+
+}
+
+const user = new User("Оля", 19);
+const userMan = new User("Дима", 19)
+user.say(); // Оля
+userMan.say(); //Дима
